cab1a83cb1ef882961c38a32333783c8c4e49ffe
nely de faria <nelly.nf54@gmail.com>
2021-08-02 16:19:29 -0300

    Cria função showQuestion

--- js/script.js
+++ js/script.js
@@ -2,3 +2,32 @@
 let currentQuestion = 0;
 let correctAnswer = 0;
 
+showQuestion();
+
+// functions
+function showQuestion() {
+    if (questions[currentQuestion]) {
+        let q = questions[currentQuestion];
+
+        let pct = Math.floor((currentQuestion / questions.length) * 100);
+        document.querySelector('.progress--bar').style.width = `${pct}%`;
+
+        document.querySelector('.scoreArea').style.display = 'none';
+        document.querySelector('.questionArea').style.display = 'block';
+
+        document.querySelector('.question').innerHTML = q.question;
+        let optionsHtml = '';
+        for (let i in q.options) {
+            optionsHtml += `<div data-op=${i} class="option"><span>${parseInt(i)+1}</span>${q.options[i]}</div>`;
+        }
+        document.querySelector('.options').innerHTML = optionsHtml;
+
+        document.querySelectorAll('.options .option').forEach(item => {
+            item.addEventListener('click', optionClickEvent);
+        });
+
+    } else {
+        // acabaram as questões
+    }
+}
+
